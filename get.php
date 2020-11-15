fdda9221f38af4663d80df21f48faf90c3ec8592
Mathusan Chandramohan <mathu99430@hotmail.com>
2020-11-15 12:00:42 -0500

    added gifs for exercices

--- get.php
+++ get.php
@@ -6,21 +6,35 @@ if (!$conn){
 	echo 'Connection error: ' . mysqli_connect_error();
 }
 
-$nickname = $_GET["nickname"];
-$study= $_GET["study"];
-$workout= $_GET["workout"];
-
-$result=mysqli_query($conn, "SELECT studytime, workouttime FROM total WHERE 1"); 
+$result=mysqli_query($conn, "SELECT name, studytime, workouttime FROM records ORDER by date"); 
 
 //storing data in array.
 $data= array();
 while ($row = mysqli_fetch_assoc($result)){
 	$data[] = $row;
 }
-$studySum=$study+intval($data[0]["studytime"]);
-$workoutSum=$workout+intval($data[0]["workouttime"]);
-
-mysqli_query($conn, "INSERT INTO records(name, studytime, workouttime) VALUES ('$nickname',  '$study', '$workout')");
-mysqli_query($conn, "UPDATE total set studytime='$studySum', workouttime='$workoutSum' WHERE 1");
-
 ?>
+<html>
+<head>
+<title>Get</title>
+</head>
+<body style="font-family:Courier">
+<?php
+for($x= 0; $x < $result->num_rows; $x++) {	
+	$rawS=intval($data[$x]["studytime"]);
+	$rawW=intval($data[$x]["workouttime"]);
+	$secondS=$rawS%60;
+	$minuteS=(($rawS-$secondS)/60)%60;
+	$hourS=((($rawS-$secondS)/60)-$minuteS)/60;
+	$secondW=$rawW%60;
+	$minuteW=(($rawW-$secondW)/60)%60;
+	$hourW=((($rawW-$secondW)/60)-$minuteW)/60;
+	$stringS=$hourS."h"." ".$minuteS."m"." ".$secondS."s";
+	$stringW=$hourW."h"." ".$minuteW."m"." ".$secondW."s";
+	echo '<p><b>'.$data[$x]["name"]."</b> "."<br>Studied: ".$stringS."<br>Worked out: ".$stringW;
+}
+?>
+<script>
+window.scrollTo(0,document.body.scrollHeight);
+</script>
+</body>
